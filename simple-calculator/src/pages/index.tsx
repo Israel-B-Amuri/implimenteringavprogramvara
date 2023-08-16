b18ca8faab34598724f70e2dec0c7e9d087adeb5
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-08-16 10:57:21 +0300

    init

--- simple-calculator/src/pages/index.tsx
+++ simple-calculator/src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Calculator from './components/calculator'
+
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-2`}
-    >
+    <main>   
       <Calculator/>
     </main>
   )
