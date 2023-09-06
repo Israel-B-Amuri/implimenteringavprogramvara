316bae43bc35cc45b67d6a585a398c54f7eb9707
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-09-06 10:17:24 +0300

    Adding functionalities to the buttons

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -2,23 +2,29 @@
 import { Container,TextField,Button,Typography,List,ListItem,ListItemText } from "@mui/material"
 import Divider from '@mui/material/Divider';
 import DeleteIcon from '@mui/icons-material/Delete';
-import React, { useState,useEffect } from 'react';
+import React, { useState} from 'react';
+import useLocalStorage from "use-local-storage";
 
 export default function Home() {
   const [todo,setTodo] = useState('')
-  const [todos,setTodos] = useState<string[]>([])
-
-  const addTodo =()=>{
+  const [todos,setTodos] = useLocalStorage<{ text:string; completed:boolean;}[]>('todos',[])
+  
+  const addTodo = () => {
 
     if(todo.trim() !== ''){
-      setTodos([...todos,todo])
+      setTodos([...todos, {text:todo,completed:false}])
       setTodo('')
-  }}
+    }
+  }
 
   
   const handleOnChange = (event:any) => setTodo(event.target.value)
-  
-  const handleCompleted = ()=> {}
+
+  const handleCompletedTask = (index:number)=>{
+   const updatedTodos = [...todos]
+   updatedTodos[index].completed = !updatedTodos[index].completed
+   setTodos(updatedTodos)
+  }
 
   const deleteTodo = (index: number) => {
     const todosCopy = [...todos]
@@ -41,16 +47,16 @@ export default function Home() {
           justifyContent:'center',
           alignItems:'center',
           gap:'2rem'
-      }}>
+        }}>
         <TextField id="standard-basic" label="Write your todo here" variant="outlined" sx={{width:'50%',}} onChange={handleOnChange}/>
         <Button variant="outlined" onClick={addTodo}>Add Todo</Button>
       </Container>
       <Container id={'todo-container'}>
         <List sx={{}} component="nav" aria-label="mailbox folders">
           {todos.map((todoItem,index) =>(
-            <div key={index + todoItem}>
-              <ListItem button>
-                <ListItemText primary={todoItem}></ListItemText>
+            <div key={index + todoItem.text}>
+              <ListItem sx={{textDecoration: todoItem.completed ? 'line-through':'none'}} onClick={()=>handleCompletedTask(index)}>
+                <ListItemText primary={todoItem.text}></ListItemText>
                 <Button onClick={() => deleteTodo(index)} variant="outlined">
                   <DeleteIcon></DeleteIcon>
                 </Button>
