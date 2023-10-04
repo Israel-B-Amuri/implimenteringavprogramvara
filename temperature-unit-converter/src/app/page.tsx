988a1ea93c069152ed73c18ff8bba5c7e62a63ae
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-10-04 13:41:43 +0300

    onChenge handler

--- temperature-unit-converter/src/app/page.tsx
+++ temperature-unit-converter/src/app/page.tsx
@@ -1,11 +1,19 @@
 'use client'
 import React,{useState} from 'react'
-import {Container} from '@mui/material'
+import {Container,Box,TextField,Button} from '@mui/material'
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 
 export default function Home() {
+  const [userInput,setUserInput] = useState('')
+
+  const handleOnChange = (event:React.ChangeEvent<HTMLInputElement>) => setUserInput(event.target.value)
   return (
     <Container>
-
+     <Box component='form' sx={{ display:'flex', justifyContent:'center',alignItems:'center',gap:'1.7rem'}}>
+      <TextField id="standard-basic" size='small' label="temperature in degrees" variant="standard"  onChange={handleOnChange}/>
+      <Button variant='contained'><ChangeCircleIcon/></Button>
+     </Box>
+     {userInput}
     </Container>
   )
 }
