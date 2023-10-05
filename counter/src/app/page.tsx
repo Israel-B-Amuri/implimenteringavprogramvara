1b4258a76f825e260b3e629987d2e3a85dd725a3
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-10-05 13:42:25 +0300

    Init

---
+++ counter/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@mui/material'
+
+export default function Home() {
+  return (
+    <Container sx={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:'space-between',padding:"24"}}>
+      <div>Hello World</div>
+    </Container>
+  )
+}
